3053a9cd8977629b21fd04151c1cecd9f290d47a
naqerl <scipunch@gmail.com>
2026-03-18 00:48:49 +0000

    dump

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+set -ex
+
 mkdir -p $HOME/.emacs.d $HOME/.config
 
-ln -s $PWD/emacs/* $HOME/.emacs.d/
-ln -s $PWD/tmux/tmux.conf $HOME/
-ln -s $PWD/.bashrc $HOME/
-ln -s $PWD/agents $HOME/.config/
-ln -s $PWD/bin/* $HOME/.local/bin/
+ln -s $PWD/emacs/* $HOME/.emacs.d/ || true
+ln -s $PWD/tmux/tmux.conf $HOME/ || true
+ln -s $PWD/.bashrc $HOME/ || true
+ln -s $PWD/agents $HOME/.config/ || true
+ln -s $PWD/bin/* $HOME/.local/bin/ || true
